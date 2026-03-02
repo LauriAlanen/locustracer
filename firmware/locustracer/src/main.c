f4be32bc30e9bced70c7276668c8cb5861567839
Lauri Alanen <e2203130@edu.vamk.fi>
2026-03-02 19:11:08 +0200

    Intialize PlatformIO Project
    - Make a custom config from N16R8

---
+++ firmware/locustracer/src/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define BLINK_GPIO 38
+
+void app_main(void)
+{
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    printf("Starting Blink Test on GPIO %d...\n", BLINK_GPIO);
+
+    while (1) {
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        
+        printf("Heartbeat: LED Toggled\n");
+    }
+}
