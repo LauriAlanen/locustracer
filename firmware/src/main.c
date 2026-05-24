8f1af4056c81d1ca9ab086484ce2a2603a4302b8
Lauri Alanen <laurialanen1@gmail.com>
2026-05-24 15:04:37 +0300

    feat(led): add neopixel heartbeat support for esp32-s3-devkitc

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -16,7 +16,12 @@
 #include "buzzer.h"
 #include "shtc3.h"
 
+#ifdef BOARD_ESP32_S3_DEVKITC_1
+#include "led_strip.h"
+#define BLINK_GPIO 48 // Use 38 for Rev 1.1 boards!
+#else
 #define BLINK_GPIO 21
+#endif
 
 static void init_mdns(void)
 {
@@ -83,6 +88,32 @@ void app_main(void)
     api_client_init();
     xTaskCreate(api_client_task, "api_client_task", 8192, NULL, 5, NULL);
 
+#ifdef BOARD_ESP32_S3_DEVKITC_1
+    // Configure the NeoPixel RGB LED on DevKitC-1
+    led_strip_handle_t led_strip;
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = BLINK_GPIO,
+        .max_leds = 1, 
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    led_strip_clear(led_strip);
+
+    uint8_t led_state = 0;
+    while (1) {
+        if (led_state) {
+            // Heartbeat: Faint blue color
+            led_strip_set_pixel(led_strip, 0, 0, 0, 16);
+            led_strip_refresh(led_strip);
+        } else {
+            led_strip_clear(led_strip);
+        }
+        led_state = !led_state;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+#else
     gpio_reset_pin(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
@@ -93,4 +124,5 @@ void app_main(void)
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+#endif
 }
